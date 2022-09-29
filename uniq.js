4a883b6d05e81c2de59602f2670c976789ca6b07
nub69 <shabik.ashab@gmail.com>
2022-09-30 00:24:11 +0600

    uniq added

--- uniq.js
+++ uniq.js
@@ -4,10 +4,15 @@
 
 
 function uniq(arr){
-    
+    let unique = [];
+    for(i=0; i < arr.length; i++){ 
+        if(unique.indexOf(arr[i]) === -1) { 
+            unique.push(arr[i]); 
+        } 
+    }
+    return unique;
 }
 
 const arr = [2,1,2,3,5,2,4,5,7,8,5,4,6,7];
 
 console.log(uniq(arr));
-
