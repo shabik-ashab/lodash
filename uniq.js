7277dd7be87caffe68435ead80ed4e3492c22483
nub69 <shabik.ashab@gmail.com>
2022-09-29 23:45:21 +0600

    initilized

---
+++ uniq.js
@@ -0,0 +1,13 @@
+// Creates a duplicate-free version of an array, using SameValueZero for
+// equality comparisons,in which only the first occurrence of each element is kept.
+// The order of result values is determined by the order they occur in the array.
+
+
+function uniq(arr){
+    
+}
+
+const arr = [2,1,2,3,5,2,4,5,7,8,5,4,6,7];
+
+console.log(uniq(arr));
+
